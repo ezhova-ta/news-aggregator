7b27c1bfcdea9cca263f9c78957963798c3698dd
Tatyana <ezhova.ta.ta@yandex.ru>
2019-05-14 15:57:10 +0300

    Изменен шаблон вывода новостей канала.

--- app/src/main/java/com/example/newsaggregator/view/news/entry/list/NewsEntryHolder.java
+++ app/src/main/java/com/example/newsaggregator/view/news/entry/list/NewsEntryHolder.java
@@ -10,7 +10,6 @@ import com.example.newsaggregator.model.entity.NewsEntry;
 
 class NewsEntryHolder extends RecyclerView.ViewHolder {
     private final TextView newsEntryTitleTextView;
-    private final TextView newsEntryLinkTextView;
     private final TextView newsEntryDescriptionTextView;
     private final TextView newsEntryPubDateTextView;
 
@@ -18,14 +17,12 @@ class NewsEntryHolder extends RecyclerView.ViewHolder {
         super(newsEntryView);
 
         newsEntryTitleTextView = newsEntryView.findViewById(R.id.newsEntryTitle);
-        newsEntryLinkTextView = newsEntryView.findViewById(R.id.newsEntryLink);
         newsEntryDescriptionTextView = newsEntryView.findViewById(R.id.newsEntryDescription);
         newsEntryPubDateTextView = newsEntryView.findViewById(R.id.newsEntryPubDate);
     }
 
     void fillView(final NewsEntry newsEntry) {
         newsEntryTitleTextView.setText(newsEntry.getTitle());
-        newsEntryLinkTextView.setText(newsEntry.getLink());
         newsEntryDescriptionTextView.setText(newsEntry.getDescription());
         newsEntryPubDateTextView.setText(newsEntry.getPubDate());
     }
